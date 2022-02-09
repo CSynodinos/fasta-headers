a27d5127b6143952ab9076265f9b08fec88d8d6f
Christos Synodinos <chris_sinodinos@outlook.com>
2022-02-09 20:04:25 +0000

    added csv parser, inherited by headers

--- nfasta_headers.py
+++ nfasta_headers.py
@@ -3,19 +3,70 @@ from __future__ import annotations
 
 from Bio import SeqIO
 import re
-from collections import defaultdict
+import pandas as pd
 
 
-class headers:
-    def __init__(self, inp :str, out: str, pattern: list, new_id: str, new_desc: str) -> None:
-        self.inp = inp
-        self.out = out
-        self.pattern = pattern
-        self.new_id = new_id
-        self. new_desc = new_desc
+class _info_parser:
+    """Parses all the information required for renaming the fasta file."""
+
+    def __init__(self, csvfl) -> None:
+        self.csvfl = csvfl
 
     @staticmethod
-    def _record_parser(record: str, switch_id: str, switch_description: str, outputfl: str, counter = None) -> None:
+    def _main_parser(fl):
+        """Parse through .csv file containing the patterns and their corresponding id and description changes.
+
+        Args:
+            * `fl` (str): Input file (.csv format).
+
+        Returns:
+            [type]: Three lists;
+            * List containing all patterns to find.
+            * List containing their corresponding new ids.
+            * List containing their corresponding new descriptions.
+        """
+
+        df = pd.read_csv(fl)
+        dict_df = df.to_dict()
+        keys = list(dict_df.keys())
+
+        pattern_key = [keys[0]]
+        id_key = [keys[1]]
+        desc_key = [keys[2]]
+
+        patterns = [dict_df[y] for y in pattern_key]
+        new_ids = [dict_df[y] for y in id_key]
+        new_desc = [dict_df[y] for y in desc_key]
+
+        patterns_dict = patterns[0]
+        new_ids_dict = new_ids[0]
+        new_desc_dict = new_desc[0]
+
+        return patterns_dict, new_ids_dict, new_desc_dict
+
+    def _pattern_parser(self):
+        """Returns a list containing the specified patterns."""
+        return list(self._main_parser(fl = self.csvfl)[0].values())
+
+    def _new_ids_parser(self):
+        """Returns a list containing the specified new ids."""
+        return list(self._main_parser(fl = self.csvfl)[1].values())
+
+    def _new_desc_parser(self):
+        """Returns a list containing the specified new descriptions."""
+        return list(self._main_parser(fl = self.csvfl)[2].values())
+
+
+class headers(_info_parser):
+    def __init__(self, inp :str, out: str, info: bool, change_id: bool, change_desc: bool, csvfl: str) -> None:
+        self.inp = inp
+        self.out = out
+        self.info = info
+        self.change_id = change_id
+        self.change_desc = change_desc
+        super().__init__(csvfl)
+
+    def _record_parser(self, record: str, switch_id: str, switch_description: str, outputfl: str, counter = None) -> None:
         """Header renaming method.
 
         Args:
@@ -42,12 +93,17 @@ class headers:
             except:
                 raise ValueError(f"counter must be of type: int if specified, not of type {type(counter).__name__}.")
 
-        record.id = f'{switch_id}{counter}'
-        record.description = f'{switch_description}{counter}'
+        if self.change_id == True:
+            record.id = f'{switch_id}{counter}'
+
+        if self.change_desc == True:
+            record.description = f'{switch_description}{counter}'
+
         SeqIO.write(record, outputfl, 'fasta')
 
-        print(f"\nHeader: {original_header} changed to {record.id}. Description changed" 
-            ""f"from {original_description} to {record.description}.")
+        if self.info == True:
+            print(f"\nHeader: {original_header} changed to {record.id}. Description changed" 
+                ""f"from {original_description} to {record.description}.")
 
     @classmethod
     def _exec_parser(cls, c: bool, rec: any, iter: int, nid: str, ndesc: str, out: str) -> None:
@@ -80,6 +136,7 @@ class headers:
             * Path to output fasta file.
         """
 
+
         with open(self.inp, "r") as fl, open(self.out, 'w') as outfl:
             headers = 0
             for line in fl:
@@ -98,23 +155,23 @@ class headers:
             iter = 1
             invalid_patterns = []
             for record in records:  # loop through records.
-                for i in self.pattern:   # nested loop through specified patterns.
+                for (i, new_id, new_desc) in zip(self._pattern_parser(), self._new_ids_parser(), self._new_desc_parser()):   # nested loop through specified patterns.
                     if not any(i in rec for rec in record_ids):
                         invalid_patterns.append(i)
                         continue
 
                     if iter > 1: # only when the first pattern in the list is iterated.
                         if re.search(i, record.id):
-                            self._exec_parser(c = count, rec = record, iter = iter, nid = self.new_id,
-                                            ndesc = self.new_desc, out = outfl)
+                            self._exec_parser(c = count, rec = record, iter = iter, nid = new_id,
+                                            ndesc = new_desc, out = outfl)
                             iter += 1
                         else:
                             continue
 
                     else:   # Run first pattern seperately.
                         if re.search(i, record.id):
-                            self._exec_parser(c = count, rec = record, iter = iter, nid = self.new_id,
-                                            ndesc = self.new_desc, out = outfl)
+                            self._exec_parser(c = count, rec = record, iter = iter, nid = new_id,
+                                            ndesc = new_desc, out = outfl)
                             iter += 1
                         else:
                             continue
@@ -135,9 +192,7 @@ class headers:
 def main():
     inp_fl = ""
     out_fl = ""
-    patterns = [""]
-    headers(inp = inp_fl, out = out_fl, pattern = patterns, 
-            new_id = "foo", new_desc = "bar").rename(count = True)
+    headers(inp = inp_fl, out = out_fl, info = True, change_id = True, change_desc = True, csvfl = "patterns.csv").rename(count = True)
 
 if __name__ == "__main__":
     main()
