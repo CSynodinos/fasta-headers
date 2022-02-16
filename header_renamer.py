9a4fc795ceca54fdbc0a2da43527f313c11f0cd4
CSynodinos <chris_sinodinos@outlook.com>
2022-02-16 21:06:45 +0000

    better docs in custom exception

--- header_renamer.py
+++ header_renamer.py
@@ -11,13 +11,13 @@ class InputflError(Exception):
 
     __module__ = 'builtins'
 
-    def __init__(self, *args):
+    def __init__(self, *args) -> None:
         if args:
             self.errmessage = args[0]
         else:
             self.errmessage = None
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         if self.errmessage:
             return '{0} '.format(self.errmessage)
         else:
